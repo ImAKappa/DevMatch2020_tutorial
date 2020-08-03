8a87e354a27cca21af1e121350e72c029d0a8b2e
Alex Kappen <alexkappen@gmail.com>
2020-08-03 01:20:33 +0000

    Cleaned up contact form routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
   get 'about', to: 'pages#about' 
-  resources :contacts
+  resources :contacts, only: :create, as: 'new_contact'
   get 'contact-us', to: 'contacts#new'
 end
